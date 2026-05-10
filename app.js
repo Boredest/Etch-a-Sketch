a934cc8749fe9f9724b4993b5fe03315d9d4c188
Boredest <19357608+Boredest@users.noreply.github.com>
2026-05-10 16:52:22 +0900

    Added JS for slider value. Later I will connect it to the grid so a user can change the grid dynamically. Added rainbow mode button.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const colors = ["red", "green", "blue", "yellow"];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const container = document.querySelector(".grid-container");
+
+const slider = document.querySelector(".slider");
+
+const sliderText = document.querySelector(".slider-value");
 
 function GenerateGrid() {
   for (let i = 0; i < 16; i++) {
@@ -13,12 +17,16 @@ function GenerateGrid() {
 
 GenerateGrid();
 
-
 const cells = document.querySelectorAll(".grid-container div");
 
 cells.forEach((cell) => {
-  cell.addEventListener("mouseover", (event) =>{
-     cell.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  cell.addEventListener("mouseover", (event) => {
+    cell.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
   });
 });
 
+slider.addEventListener("input", function () {
+  //displays string not int
+  sliderText.textContent = slider.value + " x " + slider.value;
+});
